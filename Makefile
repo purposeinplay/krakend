102b163e26adcccb2b6879af209ceff99b6d270a
kpacha <kpacha666@gmail.com>
2018-12-03 18:20:50 +0100

    ws module added to the build process

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 KRAKENDCE_REPO=https://github.com/devopsfaith/krakend-ce
-IMPORT_FILE=backend_selector.go
+KRAKENDCE_VERSION=0.7.0
 
 all: build clean
 
 build:
-		git clone ${KRAKENDCE_REPO}
-		cp ${IMPORT_FILE} krakend-ce/
-		cd krakend-ce && make docker_build_alpine && VERSION=custom make -e krakend_docker
+		git clone -b "${KRAKENDCE_VERSION}" ${KRAKENDCE_REPO}
+		cp backend_selector.go krakend-ce/.
+		cp handler_factory.go krakend-ce/.
+		cat gopkg.toml.partial >> krakend-ce/Gopkg.toml
+		cd krakend-ce && make docker_build_alpine && VERSION=${KRAKENDCE_VERSION}-custom make -e krakend_docker
 
 clean:
 		rm -rf krakend-ce
---
+++ handler_factory.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+
+	"github.com/ProvablyFair/krakend/websocket"
+	jose "github.com/devopsfaith/krakend-jose"
+	metrics "github.com/devopsfaith/krakend-metrics/gin"
+	opencensus "github.com/devopsfaith/krakend-opencensus/router/gin"
+	juju "github.com/devopsfaith/krakend-ratelimit/juju/router/gin"
+	"github.com/devopsfaith/krakend/logging"
+	router "github.com/devopsfaith/krakend/router/gin"
+)
+
+// NewHandlerFactory returns a HandlerFactory with a rate-limit and a metrics collector middleware injected
+func NewHandlerFactory(logger logging.Logger, metricCollector *metrics.Metrics, rejecter jose.Rejecter) router.HandlerFactory {
+	handlerFactory := juju.HandlerFactory
+	handlerFactory = jose.HandlerFactory(handlerFactory, logger, rejecter)
+	handlerFactory = metricCollector.NewHTTPHandlerFactory(handlerFactory)
+	handlerFactory = opencensus.New(handlerFactory)
+	handlerFactory = websocket.HandlerFactory(context.Background(), logger, handlerFactory)
+	return handlerFactory
+}
